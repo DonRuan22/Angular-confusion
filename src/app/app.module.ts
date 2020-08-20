54c7ca1fc749ff3b53abdc1d5845ca7ca78daca5
DonRuan22 <ruan_donino@hotmail.com>
2020-08-20 11:03:15 -0300

    Template Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,10 @@ import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { LoginComponent } from './login/login.component';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
 
 @NgModule({
   declarations: [
@@ -47,7 +51,11 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatCardModule,
     MatButtonModule,
     MatDialogModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule 
   ],
   entryComponents: [
     LoginComponent
