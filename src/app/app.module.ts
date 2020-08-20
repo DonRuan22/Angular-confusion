d1d2017ad2bbce7da1881f66f08c55aa9637023c
DonRuan22 <ruan_donino@hotmail.com>
2020-08-20 15:50:11 -0300

    promice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms'; 
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -55,7 +59,11 @@ import { FormsModule } from '@angular/forms';
     MatFormFieldModule, 
     MatInputModule,
     MatCheckboxModule,
-    FormsModule 
+    MatSelectModule,
+    MatSlideToggleModule,
+    FormsModule,
+    MatProgressSpinnerModule,
+    ReactiveFormsModule 
   ],
   entryComponents: [
     LoginComponent
