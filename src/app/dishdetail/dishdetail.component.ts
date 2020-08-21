6c9b41a1e9053ef10a6d46ef4af7ea4ccedebe6b
DonRuan22 <ruan_donino@hotmail.com>
2020-08-21 11:39:34 -0300

    Assigment3

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
-
 import { Params, ActivatedRoute } from '@angular/router';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Comment } from '../shared/comment'; 
+
 import { Location } from '@angular/common';
+
 
 import { switchMap } from 'rxjs/operators';
 
@@ -15,23 +19,48 @@ import { switchMap } from 'rxjs/operators';
 })
 export class DishdetailComponent implements OnInit {
 
+  @ViewChild('fform') feedbackFormDirective;
+
+  feedbackForm: FormGroup;
+  comment: Comment;
+
+  formErrors = {
+    'name': '',
+    'text': ''
+  };
+
+  validationMessages = {
+    'name': {
+      'required':      'Name is required.',
+      'minlength':     'Name must be at least 2 characters long.',
+      'maxlength':     'Name cannot be more than 25 characters long.'
+    },
+    'text': {
+      'required':      'Text is required.',
+    }
+  };
 
   dish: Dish;
 
   dishIds: string[];
   prev: string;
   next: string;
+  validated:boolean;
+
 
   
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
     .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+    this.createForm();
+    this.validated=null;
   }
 
   setPrevNext(dishId: string) {
@@ -42,6 +71,64 @@ export class DishdetailComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+  createForm(): void {
+    this.feedbackForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      text: ['', [Validators.required] ],
+      rating:'0'
+    });
+
+    this.feedbackForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); // (re)set validation messages now
+  }
+  onSubmit() {
+    this.comment = new Comment();
+    this.comment.author = this.feedbackForm.value.name;
+    this.comment.comment = this.feedbackForm.value.text;
+    this.comment.rating = this.feedbackForm.value.rating;
+    this.comment.date = new Date().toISOString(); 
+    this.dish.comments.push(this.comment);
+
+    console.log(this.comment);
+    this.feedbackForm.reset(
+      {
+        name: '',
+        text: '',
+        rating: '5'
+      }
+    );
+    this.feedbackFormDirective.resetForm();
+  }
+  
+
+  onValueChanged(data?: any) {
+    if(this.feedbackForm.status == "VALID"){
+      this.validated = true;
+    }
+    else{
+      this.validated = null;
+    }
+    if (!this.feedbackForm) { return; }
+    const form = this.feedbackForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
   }
 
 }
